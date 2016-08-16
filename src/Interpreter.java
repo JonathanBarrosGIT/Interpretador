7e6a7b17eb4b615709f0248dea87b3ce966c3cf9
Jonathan Barros <jonathan.cbar@gmail.com>
2016-08-16 11:43:29 -0300

    Comando implementado, mas ainda não tem tratamento de erro ou exceçao.

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -3,53 +3,49 @@ import java.util.Scanner;
 public class Interpreter {
 
 	public static void main(String[] args) {
-		String userInput, rs1 = "", rs2 = "";
-		int resultadoA = 0, r1 = 0, r2 = 0;
-		boolean resultadoB = false, rb1 = false, rb2 = false;
 
-		System.out.println("Digite a instruçãoo que queira realizar ou digite '?' "
-				+ "para ajuda:");
+		System.out.println("Digite a instrução que queira realizar ou digite '?' para ajuda:");
 
 		Scanner input = new Scanner(System.in);
 
-		userInput = input.nextLine();
+		String userInput = input.nextLine();
+
+		String[] command = userInput.split(" ");
 
 		if(userInput.equals("?")){
 			System.out.println("Funções disponíveis: somar, multiplicar, dividir, "
 					+ "subtrair, stringComp, numberEqual, maior, menor, and e or.");
-		}
-
-		else{
-			switch(userInput){
+		} else{
+			switch(command[0]){
 			case "somar":
-				resultadoA = Aritmetica.somar(r1, r2);
+				System.out.println(Aritmetica.somar(Integer.parseInt(command[1]), Integer.parseInt(command[2])));
 				break;
 			case "multiplicar":
-				resultadoA = Aritmetica.multiplicar(r1, r2);
+				System.out.println(Aritmetica.multiplicar(Integer.parseInt(command[1]), Integer.parseInt(command[2])));
 				break;
 			case "dividir":
-				resultadoA = Aritmetica.dividir(r1, r2);
+				System.out.println(Aritmetica.dividir(Integer.parseInt(command[1]), Integer.parseInt(command[2])));
 				break;
 			case "subtrair":
-				resultadoA = Aritmetica.subtrair(r1, r2);
+				System.out.println(Aritmetica.subtrair(Integer.parseInt(command[1]), Integer.parseInt(command[2])));
 				break;
 			case "and":
-				resultadoB = Booleano.and(rb1, rb2);
+				System.out.println(Booleano.and(Boolean.parseBoolean(command[1]), Boolean.parseBoolean(command[2])));
 				break;
 			case "or":
-				resultadoB = Booleano.or(rb1, rb2);
+				System.out.println(Booleano.or(Boolean.parseBoolean(command[1]), Boolean.parseBoolean(command[2])));
 				break;
 			case "stringComp":
-				resultadoB = BitWise.stringComp(rs1, rs2);
+				System.out.println(BitWise.stringComp(command[1], command[2]));
 				break;
 			case "igual":
-				resultadoB = BitWise.numberEqual(r1, r2);
+				System.out.println(BitWise.numberEqual(Integer.parseInt(command[1]), Integer.parseInt(command[2])));
 				break;
 			case "maior":
-				resultadoB = BitWise.numberR1Great(r1, r2);
+				System.out.println(BitWise.numberR1Great(Integer.parseInt(command[1]), Integer.parseInt(command[2])));
 				break;
 			case "menor":
-				resultadoB = BitWise.numberR1Lower(r1, r2);
+				System.out.println(BitWise.numberR1Lower(Integer.parseInt(command[1]), Integer.parseInt(command[2])));
 				break;
 			default:
 				System.out.println("Instrução não encontrada.");
